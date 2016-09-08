af7563a333ab58fb2f0f1439c26e97a1377c3650
Wessel Stoop <wstoop@applejack.science.ru.nl>
2016-09-08 17:10:33 +0200

    Created largest part of server functionality

--- scripts/timblserver-command.sh
+++ scripts/timblserver-command.sh
@@ -1,1 +1,2 @@
-timblserver -i ../data/toxic_chat.igtree -a1 -S 7078
+timblserver -i ../data/toxic_chat.igtree -a1 -S 7078 &
+timblserver -i ../data/normal_chat.igtree -a1 -S 7079 &
--- scripts/toxic_player_server.py
+++ scripts/toxic_player_server.py
@@ -5,39 +5,45 @@ import socket
 class ToxicPlayerServer(object):
 
     connected_to_timbl = False
-    timblsocket = None
+
+    chat_sockets = {}
+    ports = {'toxic': 7078, 'normal': 7079}
 
     def predict(self,text):
 
         if not self.connected_to_timbl:
             self.connect_to_timbl()
 
-        answer = self.send_to_timbl(text)
-        return answer
+        toxic_answer = self.send_to_timbl('toxic',text)
+        normal_answer = self.send_to_timbl('normal',text)
+
+        return toxic_answer+'\t'+normal_answer
 
     predict.exposed = True
 
-    def connect_to_timbl(self, port=7078, host='', retry=20, interval=1):
+    def connect_to_timbl(self, host='', retry=20, interval=1):
         """ Connect to a Timbl server"""
 
-        self.timblsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        for socket_label in ['toxic','normal']:
 
-        while retry > 0:
-            retry -= 1
+            self.chat_sockets[socket_label] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-            try:
-                self.timblsocket.connect((host, int(port)))
-                self.timblsocket.recv(1024)  # Ignore welcome messages
-            except socket.error:
-                time.sleep(interval)
-                continue
+            while retry > 0:
+                retry -= 1
 
-            self.connected_to_timbl = True
-            break
+                try:
+                    self.chat_sockets[socket_label].connect((host, int(self.ports[socket_label])))
+                    self.chat_sockets[socket_label].recv(1024)  # Ignore welcome messages
+                except socket.error:
+                    time.sleep(interval)
+                    continue
+
+                self.connected_to_timbl = True
+                break
 
         return self.connected_to_timbl
 
-    def send_to_timbl(self, words_to_classify):
+    def send_to_timbl(self, socket_label, words_to_classify):
 
         number_of_words = len(words_to_classify.split())
         while number_of_words < 20:
@@ -46,10 +52,8 @@ class ToxicPlayerServer(object):
 
         message = 'c '+words_to_classify+' ?\n'
 
-        print(message)
-
-        self.timblsocket.sendall(message.encode())
-        return self.timblsocket.recv(1024).decode()[10:-2]
+        self.chat_sockets[socket_label].sendall(message.encode())
+        return self.chat_sockets[socket_label].recv(1024).decode()[10:-2]
 
 cherrypy.config.update({'server.socket_port': 7077})
 cherrypy.quickstart(ToxicPlayerServer())
